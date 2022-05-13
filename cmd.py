0234e107111f232fc1fdac14012632d0347e666a
A R <89004780+akrtkk@users.noreply.github.com>
2022-05-13 20:52:01 +0530

    Delete cmd.py

--- cmd.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system('cls')
-os.system('echo Microsoft Windows [Version 10.0.19042.1165]')
-os.system('echo (c) Microsoft Corporation. All rights reserved.')
-os.system(':goto set /p cmd="C:\Users\%USERNAME%>" %cmd% goto goto')
